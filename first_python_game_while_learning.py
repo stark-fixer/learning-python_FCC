5996eb208eb0d8dffc31943b697dd9acf89931bb
stark-fixer <utkarshitjobs@gmail.com>
2026-05-18 22:41:09 +0530

    MY FIRST PYTHON GAME(ROCK,PAPER AND SCISSORS
    LEARNED THE FOLLOWING TOPICS:
    1.fstring
    2.else,elif  condition
    3.Refractoring and nested if
    4.Accessing dictionaries values
    5.Testing Game

--- first_python_game_while_learning.py
+++ first_python_game_while_learning.py
@@ -7,6 +7,30 @@ def get_choise(): #making of our function
     choices={'player':player_choise, 'computer':computer_choise}
     return choices
 
+def check_win(player,computer):
+    print(f"You chose {player},computer chose {computer}") #using fstring
+    #using NESTED IFs:-
+    if player==computer:                                     
+        return"It's a tie!"
+    if player=='rock':
+        if computer=='scissors':
+          return('Rock smashes scissors! You win!')
+        else:
+          return('Paper covers the rock! You lose')  
+    if player=='paper':
+        if computer=='rock':
+          return('Paper covers rock! You win!')
+        else:
+          return('scissors cut papers! You lose')  
+    if player=="scissors":
+        if computer=='paper':
+          return('Scissors cut paper! You win!')
+        else:
+          return('Rock smashes scissors! You lose')        
 
+#KNOWING THE RESULTS
+choises=get_choise()
+result=check_win(choises['player'],choises['computer'])#it will get the user's and the computer choise and will check the results based on our nested if commands[also learn how to acess disctionary values]
+print(result)#printing the final result.   
 
 
