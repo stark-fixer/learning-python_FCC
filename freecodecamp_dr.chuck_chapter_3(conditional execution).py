38975ca40786dabac00121e28a986ad320fa3535
stark-fixer <utkarshitjobs@gmail.com>
2026-07-28 18:24:05 +0530

    Create freecodecamp_dr.chuck_chapter_3(conditional execution).py
    TIME FLOWS

---
+++ freecodecamp_dr.chuck_chapter_3(conditional execution).py
@@ -0,0 +1,16 @@
+#if statement
+#indentation.
+x=5
+if x>5:
+    print("hello")      #Indented statement
+if x == 5:
+    print("tata")
+
+
+#Two way decision with else:
+x=4
+if x>2:
+    print("bigger")
+else:                   #the else statement
+    print("Smaller")
+print("all done")
